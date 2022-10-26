f84a35c07ee27c6bba9e43eff06a235a4f8eef06
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2022-10-26 16:34:20 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio79/Program.cs
@@ -0,0 +1,15 @@
+﻿
+class Ejercicio79
+{
+    static void main()
+    {
+        HashSet<string> diccio = new HashSet<string>(); 
+        diccio.Add("Hola");
+        diccio.Add("hasta luego");
+
+        if (diccio.Contains("Adios"))
+            Console.WriteLine("Adios si esta");
+        if (diccio.Contains("Hola"))
+            Console.WriteLine("Hola si esta");
+    }
+}
